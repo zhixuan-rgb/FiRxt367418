ad54efc9b9fcb73a1bfaede38812fbff18cec5a4
Zhi Xuan <zhixuan@ever-technologies.com>
2026-04-02 16:51:36 +0800

    Create layout.tsx

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "@/components/layout/navbar";
+import { Footer } from "@/components/layout/footer";
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Navbar />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  );
+}
